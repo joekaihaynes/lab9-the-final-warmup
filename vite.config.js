4736213fa0637eb7810dc46663aa5b826e9106e4
Joe Haynes <JoeHaynes@sandiego.edu>
2025-11-05 12:57:03 -0800

    refactor: made it so that the tests deploy from tests not src

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',
+  root: './src', // Dev server serves from src
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './tests/unitTest/setup.js',
+    setupFiles: '../tests/unitTest/setup.js', // relative to root
+    include: ['../tests/unitTest/**/*.test.js'], // include tests outside src
     pool: 'vmThreads',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html', 'lcov'],
-      include: ['src/**/*.{js,ts}'],
+      include: ['**/*.{js,ts}'],
     },
-  }
+  },
 });
