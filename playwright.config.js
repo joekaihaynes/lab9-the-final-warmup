7d22c47c14a9a441b11ca0fbaa7ce087e60467e4
Joe Haynes <joehaynes@sandiego.edu>
2025-11-05 12:34:58 -0800

    feature: add playwright config file

---
+++ playwright.config.js
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests/e2eTesting',
+  timeout: 30 * 1000,
+  retries: 0,
+  reporter: [['list'], ['html', { open: 'never' }]],
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 5000,
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
